061dac57bcc69db64cb63089835c28179b28fbdc
Juan-DavidP <juandavidpelaezvilla@gmail.com>
2024-04-05 12:43:57 -0500

    sesion 8

--- Sesion 8/sesion8.py
+++ Sesion 8/sesion8.py
@@ -23,28 +23,37 @@
 
 
 # Actividad 1
-print("Actividad 1")
+# print("Actividad 1")
 # Vamos a realizar un algoritmo que nos calcule la serie de Fibonacci.
 # La serie o secuencia de Fibonacci comienza con los números 0 y 1 y 1, y a partir de allí es posible calcular el siguiente valor como la suma de los dos valores anteriores. 
 # Por ejemplo, 1+1=2, 1+2=3, 2+3=5, etc. Así, los primeros números de la secuencia son: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, ...
 # Creemos un programa que a partir de un número N ingresado, imprima los primeros N números de la serie de Fibonacci.
 
-numeroIngresado = int(input("Ingrese el número hasta el que desea generar la serie de Fibonacci: "))
-num_uno = 0
-num_dos = 1
-sumatoria = 0
+# numeroIngresado = int(input("Ingrese el número hasta el que desea generar la serie de Fibonacci: "))
+# num_uno = 0
+# num_dos = 1
+# sumatoria = 0
 
-for i in range(numeroIngresado):
-    print(sumatoria)
-    sumatoria = num_uno + num_dos
-    num_uno = num_dos
-    num_dos = sumatoria
+# for i in range(numeroIngresado):
+#     print(sumatoria)
+#     sumatoria = num_uno + num_dos
+#     num_uno = num_dos
+#     num_dos = sumatoria
 
 # Actividad 2
 # print("actividad2")
 #Escribe el código usando break que reciba una palabra e imprima el número de letras que tiene y las letras hasta que, o bien termine la palabra o encuentre la letra a. .
 
 # Para ejecutar esta actividad, poner en comentario el resto del codigo fuente.
+
+# palabra = input("Ingrese la palabra a la cual desea contar sus letras: ")
+# # tamañoPalabra = len(palabra)
+# # print(tamañoPalabra)
+# print((f"El tamaño de la palabra es de {len(palabra)} letras"))
+# for i in palabra:
+#     if i == "a":
+#         break
+#     print(i)
 
 
 
@@ -54,10 +63,33 @@ for i in range(numeroIngresado):
 
 # Para ejecutar esta actividad, poner en comentario el resto del codigo fuente.
 
+# contadorPositivos = 0
+# contadorNegativos = 0
+# contadorNeutros = 0
+# for i in range(10):
+#     numeroIngresado = int(input("Ingrese el número que desea determinar si es positivo, negativo o neutro : "))
+#     if numeroIngresado > 0:
+#         contadorPositivos += 1
+#     elif numeroIngresado == 0:
+#         contadorNeutros +=1
+#     else:
+#         contadorNegativos += 1
+# print(f"La cantidad de números positivos son: {contadorPositivos}, La cantidad números negativos son: {contadorNegativos} y la cantidad de números neutros son: {contadorNeutros}")
 
 
 # Actividad 4
-print("actividad4")
+# print("actividad4")
 #Usando tanto while como for, escribe el codigo que pida números al usuario hasta que este ingrese -1 y que retorne el factorial de cada número ingresado usando un ciclo Para (For).
 
 # Para ejecutar esta actividad, poner en comentario el resto del codigo fuente.
+
+factorial = 1
+contador = 0
+numeroIngresado = int(input("Ingrese un número para calcular su factorial: "))
+while numeroIngresado != -1:
+    for i in range(1, numeroIngresado + 1):
+        factorial = i * factorial
+    print(factorial)
+    print("Para salir escriba -1")
+    factorial = 1
+    numeroIngresado = int(input("Ingrese un número para calcular su factorial: "))
